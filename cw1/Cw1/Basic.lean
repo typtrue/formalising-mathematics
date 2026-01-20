ec3cd24f547064e4cd9d5c253da2bfab95cf702a
Dan <149019458+typtrue@users.noreply.github.com>
2026-01-20 14:47:16 +0000

    setup

--- cw1/Cw1/Basic.lean
+++ cw1/Cw1/Basic.lean
@@ -1,1 +1,17 @@
-def hello := "world"
+import Mathlib.Tactic
+
+-- Bolzano-Weierstrauss for (xₙ) in ℝ
+
+def is_peak (a : ℕ → ℝ) (n : ℕ) :=
+  ∀ (m : ℕ) > n, a m ≤ a n
+
+def has_limit () :=
+  sorry
+
+-- todo figure out how to make a subsequence + setup sequences
+-- properly.
+-- maybe ask on edstem / go to office hour if there is one
+theorem bolzano_weierstrauss (a : ℕ → ℝ) : ∃ (i : ℕ → ℕ),
+    by {
+  sorry
+}
