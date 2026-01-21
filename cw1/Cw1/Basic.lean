cc428e13597b3ad33a645affc43cb4fc7df14240
typtrue <typtrue@github.com>
2026-01-21 17:54:46 +0000

    finish definitions

--- cw1/Cw1/Basic.lean
+++ cw1/Cw1/Basic.lean
@@ -10,48 +10,48 @@ set_option linter.style.commandStart false
 def is_monotone_increasing (a : ℕ → ℝ) : Prop :=
   ∀ (n m : ℕ), n ≤ m → a n ≤ a m
 
-
 /-- define an upper bound -/
 def is_upper_bound (a : ℕ → ℝ) (K : ℝ) : Prop :=
   ∀ (n : ℕ), K > a n
-
 
 /-- define what it means for a sequence to be bounded above -/
 def is_bounded_above (a : ℕ → ℝ) : Prop :=
   ∃ (K : ℝ), is_upper_bound a K
 
-
 /-- define monotone decreasing sequences -/
 def is_monotone_decreasing (a : ℕ → ℝ) : Prop :=
   ∀ (n m : ℕ), n ≤ m → a m ≤ a n
-
 
 /-- define a lower bound -/
 def is_lower_bound (a : ℕ → ℝ) (K : ℝ) : Prop :=
   ∀ (n : ℕ), a n ≤ K
 
-
 /-- define what it means for a sequence to be bounded below -/
 def is_bounded_below (a : ℕ → ℝ) : Prop :=
   ∃ (K : ℝ), is_lower_bound a K
 
-
 /--
 define bounded monotone sequences
+specifically, sequences that are both monotone increasing and bounded above,
+or sequences that are both monotone decreasing and bounded below
 -/
 def is_bounded_monotone (a : ℕ → ℝ) : Prop :=
   (is_monotone_increasing a ∧ is_bounded_above a) ∨ (is_monotone_decreasing a ∧ is_bounded_below a)
 
+/-- define whether L is a limit -/
 def is_limit (a : ℕ → ℝ) (L : ℝ) : Prop :=
   ∀ ε > 0, ∃ (N : ℕ), ∀ n > N, |a n - L| < ε
 
+/-- define whether a sequence converges to a limit -/
 def is_convergent (a : ℕ → ℝ) : Prop :=
   ∃ (L : ℝ), is_limit a L
 
+/-- define whether x is a supremum -/
 def is_supremum (a : ℕ → ℝ) (x : ℝ) : Prop :=
   is_upper_bound a x ∧
-  ∀ (K : ℝ), is_upper_bound a K → K ≥ x
+  ∀ (K : ℝ), is_upper_bound a K → x ≤ K
 
+/-- define whether x is an infimum -/
 def is_infimum (a : ℕ → ℝ) (x : ℝ) : Prop :=
   is_lower_bound a x ∧
   ∀ (K : ℝ), is_lower_bound a K → K ≤ x
@@ -73,7 +73,6 @@ axiom bounded_above_has_supremum (a : ℕ → ℝ) :
 axiom bounded_above_has_infimum (a : ℕ → ℝ):
   is_bounded_below a → ∃ (s : ℝ), is_infimum a s
 
-
 /-- Any monotone increasing sequence in ℝ with a supremum converges to its supremum. -/
 lemma mono_inc_conv_to_sup (a : ℕ → ℝ) (L : ℝ) (hMono : is_monotone_increasing a) (hSup : is_supremum a L) : is_limit a L := by
 {
@@ -94,8 +93,6 @@ theorem monotone_conv (a : ℕ → ℝ) (hBM : is_bounded_monotone a) : is_conve
   | inl hInc =>
     -- monotone increasing case
     rcases hInc with ⟨hMono, hBound⟩
-
-
     sorry
   | inr hDec =>
     -- monotone decreasing case
